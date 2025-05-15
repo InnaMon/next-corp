638e9ccfbbc70af3bc38138f7f6ae3d3100f0df4
Inna Mon <inna.mon@gmail.com>
2025-05-15 11:21:31 -0500

    style pages

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -1,3 +1,12 @@
+import performanceImg from "/public/performance.jpg";
+import Hero from "@/components/hero";
+
 export default function PerformancePage() {
-  return <div>Performance Page</div>;
+  return (
+    <Hero
+      title="We serve high performance applications"
+      imgData={performanceImg}
+      imgAlt="welding"
+    />
+  );
 }
--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,3 +1,12 @@
+import reliabilityImg from "/public/reliability.jpg";
+import Hero from "@/components/hero";
+
 export default function ReliabilityPage() {
-  return <div>Reliability Page</div>;
+  return (
+    <Hero
+      title="Super high reliability hosting"
+      imgData={reliabilityImg}
+      imgAlt="welding"
+    />
+  );
 }
